fa781989409d8e48276b95141b8d6d5093668da6
Anton Gunov <anton@gunov.name>
2017-08-08 13:07:37 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pages Boilerplate
+
+Boilerplate for prototyping web pages using Gulp, PostCSS, and Webpack.
+
+## License
+
+The code is available under the [MIT License](LICENSE).
---
+++ gulp/tasks/default.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+
+/**
+ * Gulp tasks
+ */
+
+gulp.task('default:production', gulp.series(
+  'clean',
+  gulp.parallel(
+    'copy',
+    'pug',
+    'sass',
+    'webpack:run'
+  )
+));
+
+gulp.task('default:development', gulp.series(
+  'clean',
+  gulp.parallel(
+    'copy',
+    'pug',
+    'sass',
+    'webpack:watch'
+  ),
+  gulp.parallel(
+    'watch',
+    'browser-sync:init'
+  )
+));
+
+gulp.task('default', gulp.series(
+  `default:${process.env.NODE_ENV}`
+));
