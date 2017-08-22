d9d40ebe9e8058e9c349c0915cf0b94a15f95cea
Anton Gunov <anton@gunov.name>
2017-08-22 14:39:20 +0700

    Setup pretty URLs

--- gulp/tasks/browser-sync.js
+++ gulp/tasks/browser-sync.js
@@ -2,12 +2,6 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
 /**
- * Gulp plugins
- */
-
-const { log, colors } = require('gulp-util');
-
-/**
  * Gulp tasks
  */
 
@@ -21,24 +15,14 @@ gulp.task('browser-sync:init', (done) => {
     server: {
       baseDir: 'build/',
       index: 'home.html',
+      serveStaticOptions: {
+        extensions: [ 'html' ]
+      }
     },
     open: false,
     ui: false,
     notify: false,
-    logLevel: 'silent',
-  }, (err, bs) => {
-    if (err) {
-      console.error(err.stack || err);
-      return done();
-    }
-
-    const module = colors.blue('browser-sync');
-    const port = colors.green(bs.options.getIn(['port']));
-
-    log(`[${module}] listening on port ${port}`);
-
-    return done();
-  });
+  }, done());
 
   gulp.watch('build/**/*', gulp.series('browser-sync:reload'));
 });
