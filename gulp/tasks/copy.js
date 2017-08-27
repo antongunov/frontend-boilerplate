88133c969091f0463947edb42956968e8ca04758
Anton Gunov <anton@gunov.name>
2017-08-27 19:43:50 +0700

    Improved copy Gulp task

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,10 +1,17 @@
+const copyTaskNames = [];
+
+const createCopyTask = ($, subName, from, to) => {
+  const name = `copy:${subName}`;
+  $.gulp.task(name, (done) => {
+    $.gulp.src($.dir.pages(from), { since: $.gulp.lastRun(name) })
+      .pipe($.gulp.dest($.dir.build(to)));
+    return done();
+  });
+  copyTaskNames.push(name);
+};
+
 module.exports = ($) => {
-  $.gulp.task('copy:root', () => $.gulp.src($.dir.pages('*.!(pug)'), { since: $.gulp.lastRun('copy:root') })
-    .pipe($.gulp.dest($.dir.build())));
-  $.gulp.task('copy:fonts', () => $.gulp.src($.dir.assets('fonts/*'), { since: $.gulp.lastRun('copy:fonts') })
-    .pipe($.gulp.dest($.dir.build('assets/fonts/'))));
-  $.gulp.task('copy:assets', $.gulp.parallel(
-    'copy:root',
-    'copy:fonts'
-  ));
-}
+  createCopyTask($, 'root', '*.!(pug)', '/');
+  createCopyTask($, 'fonts', 'fonts/*', 'assets/fonts/');
+  $.gulp.task('copy:assets', $.gulp.parallel(copyTaskNames));
+};
