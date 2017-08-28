8480fa737975aaa154d4393a0267a9f0d0c49425
Anton Gunov <anton@gunov.name>
2017-08-28 19:17:58 +0700

    Support for cssnano

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,22 +1,31 @@
 const autoprefixer = require('autoprefixer');
-const normalize = require('postcss-normalize');
+const cssnano = require('cssnano');
 const fontMagician = require('postcss-font-magician');
+const normalize = require('postcss-normalize');
 
 module.exports = (gulp, options, plugins) => {
+  const postcssPlugins = [
+    normalize(),
+    fontMagician({
+      hosted: [
+        options.dir.assets('fonts/'),
+      ],
+    }),
+    autoprefixer({
+      cascade: false,
+    }),
+  ];
+
+  if (!options.isDev) {
+    postcssPlugins.push(cssnano({
+      preset: 'default',
+    }));
+  }
+
   gulp.task('sass', () => gulp.src(options.dir.assets('sass/main.scss'))
     .pipe(plugins.plumber())
     .pipe(plugins.sass())
-    .pipe(plugins.postcss([
-      normalize(),
-      fontMagician({
-        hosted: [
-          options.dir.assets('fonts/'),
-        ],
-      }),
-      autoprefixer({
-        cascade: false,
-      }),
-    ]))
+    .pipe(plugins.postcss(postcssPlugins))
     .pipe(gulp.dest(options.dir.build('assets/css/'))));
   if (options.isDev) {
     gulp.watch(options.dir.assets('sass/**/*.scss'), gulp.series('sass'));
