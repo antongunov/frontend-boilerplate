26ed15fd38660515ea3d391af1bc5097e8f1b7f6
Anton Gunov <anton@gunov.name>
2017-08-29 11:56:37 +0700

    Support for UglifyJS3

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,6 +1,7 @@
 module.exports = (gulp, options, plugins) => {
   gulp.task('js', () => gulp.src(options.dir.assets('js/**/*.js'))
     .pipe(plugins.concat('main.js'))
+    .pipe(plugins.if(!options.isDev, plugins.uglify()))
     .pipe(gulp.dest(options.dir.build('assets/js/'))));
   if (options.isDev) {
     gulp.watch(options.dir.assets('js/**/*.js'), gulp.series('js'));
